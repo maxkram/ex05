b4754029ecaa89d5f5e8b793c205b9016524253b
Maksim Kramer <mkramer@c1r7p12.42quebec.com>
2024-08-31 01:38:49 -0400

    add all 3

---
+++ cpp_module00/Warlock.cpp
@@ -0,0 +1,30 @@
+#include "Warlock.hpp"
+
+Warlock::Warlock(const string& name, const string& title)
+{
+    _name = name;
+    _title = title;
+    cout << name << ": This looks like another boring day." << endl;
+}
+
+const string& Warlock::getName() const{
+    return _name;
+}
+
+const string& Warlock::getTitle() const{
+    return _title;
+}
+
+void Warlock::setTitle(const string& title){
+    _title = title;
+}
+
+void Warlock::introduce() const {
+    cout << _name << ": I am " << _name << ", " << _title << "!" << endl;
+}
+
+Warlock::~Warlock()
+{
+    cout << _name << ": My job here is done!" << endl;
+}
+
